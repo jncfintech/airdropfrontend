7c3d54b7c837fa37400ccf189d35743b14733b22
jncfintech <115144435+jncfintech@users.noreply.github.com>
2022-10-07 20:52:14 +0530

    first build

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance =  axios.create({
-        baseURL: 'http://localhost:8000',
+        // baseURL: 'http://localhost:8000',
+        baseURL:'http://35.78.65.206:8000/'
         
 });
 
